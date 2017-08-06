d8f73e9fb72eb49377d978bed75251ad9c0f5279
devedge <devedge@outlook.com>
2017-08-06 11:29:43 -0400

    Working simple TCP client

--- GoTCPChat/tcp-client.go
+++ GoTCPChat/tcp-client.go
@@ -1,2 +1,27 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, _ := net.Dial("tcp", "127.0.0.1:8081") // connect to this socket
+
+	// loop forever, until ctrl-c
+	for {
+		// read user input
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		text, _ := reader.ReadString('\n')
+
+		// send to socket
+		fmt.Fprintf(conn, text+"\n")
+
+		// listen for reply
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+	}
+}
--- GoTCPChat/tcp-server.go
+++ GoTCPChat/tcp-server.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Launching server...")
+	ln, _ := net.Listen("tcp", ":8081") // listen on all interfaces
+	conn, _ := ln.Accept()              // accept connection on port
+
+	// loop forever, until ctrl-c
+	for {
+		// listen for message to process, ending in newline
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		// output received message
+		newmessage := strings.ToUpper(message)
+		// send new string back to client
+		conn.Write([]byte(newmessage + "\n"))
+	}
+}
